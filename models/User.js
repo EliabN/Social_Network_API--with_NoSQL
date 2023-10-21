b17e5e0e77b943f193535ab6b9bc90f177bdc0f0
eliabn <30064396@tafe.wa.edu.au>
2023-10-22 00:07:37 +1030

    Create User model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,39 @@
 // User Model
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thoughts');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trimmed: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trimmed: true,
+
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema]
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property `friendCount` that gets the amount of friend of the user
+friendCount.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+
+const User = model('user', userSchema);
+
+module.exports = User;
