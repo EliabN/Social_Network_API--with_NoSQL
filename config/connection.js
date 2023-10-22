86c906c0ea5a5f2a2aeb74deff40fc2c57df5a67
eliabn <30064396@tafe.wa.edu.au>
2023-10-22 12:26:49 +1030

    Updated connection.js and created index.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,9 @@
 // Connection
+// The Mongoose module's default connection.
+const { connect, connection } = require('mongoose');
+
+// Opens Mongoose's default connection to MongoDB
+connect('mongodb://127.0.0.1:27017/usersPosts');
+
+// Export
+module.exports = connection;
