e82ee894e7870ebb6fb01d44bf69ca7a7a3673a1
eliabn <30064396@tafe.wa.edu.au>
2023-10-25 12:37:46 +1030

    Created userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,3 +1,15 @@
 // userRoutes
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
 
 module.exports = router;
